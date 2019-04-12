115670335991685d1f58dac1b880408ff59c89ba
ivanoid256 <ivanoid256@india.com>
2019-04-12 18:50:23 +0500

    Navigation Component created. onSearch event double fires issue - need to be fixed later!

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="header">
     <Title title="The best video footages from top free footage sources!" />
-    <!-- <Navigation/> -->
+    <Navigation @on-search="onSearch" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import Title from "@/components/Title.vue";
+import Navigation from "@/components/Navigation.vue";
 
 @Component({
   components: {
-    Title
+    Title,
+    Navigation
   }
 })
 export default class Header extends Vue {
   @Prop() private msg!: string;
+
+  onSearch(search_val: string) {
+    console.log(search_val);
+  }
 }
 </script>
 
---
+++ src/components/Navigation.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="navigation_cntr">
+    <div class="menu_cntr">
+      <ul class="menu">
+        <li class="menu-item">
+          <router-link to="/">Home</router-link>
+        </li>
+        <li class="menu-item">
+          <router-link to="/about">About</router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="search_cntr">
+      <el-input @keypress.native.enter="searchInputChanged"
+        v-model="searchInput" placeholder="Search footages by some keywords"
+        class="search_input">
+        <el-button @click="searchInputChanged"
+          slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Emit } from "vue-property-decorator";
+
+@Component
+export default class Navigation extends Vue {
+  @Prop() private title!: string;
+  searchInput: string = "";
+
+  @Emit("on-search")
+  searchInputChanged(e: Event) {
+    // if (e.type != "keypress" || e.target.tagName == "INPUT") {
+    // console.log(e, this.searchInput);
+    // }
+    return this.searchInput;
+  }
+
+  onBlur(e: Event) {
+    console.log(e);
+  }
+}
+</script>
+
+<style scoped>
+.navigation_cntr {
+  display: table;
+}
+
+.menu_cntr {
+  display: table-cell;
+  width: 1%;
+}
+
+.menu {
+  white-space: nowrap;
+  padding-left: 10px;
+}
+
+.menu-item {
+  display: inline;
+  margin-right: 5px;
+}
+
+.search_cntr {
+  display: table-cell;
+  width: 100%;
+  padding-right: 5px;
+}
+</style>
