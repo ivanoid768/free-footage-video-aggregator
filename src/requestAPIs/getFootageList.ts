7425a2efd987600bdf8800a8257d6443727fe0f8
ivanoid256 <ivanoid256@india.com>
2019-04-19 15:44:12 +0500

    getFootages function created to request footages from sources Pixabay.com and Pexels.com

--- src/requestAPIs/getFootageList.ts
+++ src/requestAPIs/getFootageList.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosPromise } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosPromise, AxiosResponse } from 'axios';
 
 interface SourceAxiosInstance extends AxiosInstance {
 	name: string;
@@ -42,3 +42,104 @@ function fromPexels() {
 
 	return axi;
 }
+
+interface SourceFootageResponse {
+	footages: Array<any>;
+	total: number;
+	name: string;
+}
+
+function getPixabayFootages(page = 1, per_page = 20, query = '') {
+	const axi = fromPixabay();
+
+	let params: any = {
+		page: page,
+		per_page: per_page
+	}
+
+	if (query) {
+		params.q = query;
+	}
+
+	return axi.request({
+		params: params
+	}).then((resp: AxiosResponse<any>): SourceFootageResponse => {
+		console.log(axi.name, resp.data.hits[0].videos.tiny.url)
+		// let name = hit.tags.replace(/,/ig,' ')
+		// name = name.charAt(0).toUpperCase() + name.slice(1)
+		return {
+			name: axi.name,
+			footages: resp.data.hits,
+			total: resp.data.total
+		}
+	})
+
+}
+
+function getPexelsFootages(page = 1, per_page = 20, query = '') {
+	const axi = fromPexels();
+
+	const params: any = {
+		max_duration: 30,
+		page: page,
+		per_page: per_page
+	}
+	if (!query) {
+		axi.defaults.baseURL = 'https://api.pexels.com/videos/popular';
+	} else {
+		params.query = query;
+	}
+
+	return axi.request({
+		params: params
+	}).then((resp: AxiosResponse<any>): SourceFootageResponse => {
+		console.log(resp.data.videos[0].video_files[1].link)
+		// let name = video.url.match(/((?:(?![/])[\S])+)-\d+$/i)[1].replace(/-/ig, ' ');
+		// name = name.charAt(0).toUpperCase() + name.slice(1)
+		return {
+			name: axi.name,
+			footages: resp.data.videos,
+			total: resp.data.total_results
+		};
+	})
+
+}
+
+interface FootageListItem {
+	url: string,
+	name: string
+}
+
+function getFootages(page: number = 1, per_page: number = 20, query: string = ''): Promise<FootageListItem[]> {
+	return Promise.all([
+		getPexelsFootages(page, per_page, query),
+		getPixabayFootages(page, per_page, query)
+	]).then((footResps: Array<SourceFootageResponse>) => {
+
+		let footages: FootageListItem[] = [];
+
+		for (let i = 0; i < per_page; i++) {
+			footResps.forEach(f => {
+				if (!f.footages[i]) return 1;
+
+				let footage: FootageListItem = {
+					name: '',
+					url: ''
+				};
+				if (f.name == 'pexels') {
+					footage.url = f.footages[i].video_files[1].link;
+					let name = f.footages[i].url.match(/((?:(?![/])[\S])+)-\d+$/i)[1].replace(/-/ig, ' ');
+					footage.name = name.charAt(0).toUpperCase() + name.slice(1)
+				} else if (f.name == 'pixabay') {
+					footage.url = f.footages[i].videos.tiny.url;
+					let name = f.footages[i].tags.replace(/,/ig, ' ');
+					footage.name = name.charAt(0).toUpperCase() + name.slice(1)
+				}
+
+				footages.push(footage);
+			})
+		}
+		return footages;
+	})
+}
+
--- system_arch/plan.md
+++ system_arch/plan.md
@@ -9,7 +9,7 @@
 5. 2 source Pexels? API interface
 5.1 Pexels API playground - 1 h 12:00 - 13:00 
 5.2 Pexels FootageList request - 30 m
-5.3 GetFootages function - 
+5.3 GetFootages function - 1.5 h 14:12 - 15:42
 6. FootageListItem Component
 7. FootageList Component
 8. FootageDetails Component
